12cd14eea9bdd4f6e475bf35a2d85364c3c21f4d
Amila Uduwerella <amila.uduwerella@gmail.com>
2017-05-01 23:10:17 +0530

    May Day

---
+++ 01052017/MayDay.java
@@ -0,0 +1,14 @@
+public class MayDay
+{
+	public static void main( String[] args )
+	{
+		System.out.println("\n");
+		System.out.println("+=======================================================================+");
+		System.out.println("|\t Herath Mudiyanselage Kavinu Nethaka Bandara Uduwerella \t|");
+		System.out.println("|\t\t	No 37/2B, Gangarama Road,	\t\t|");
+		System.out.println("|\t\t\t	Werahera	\t\t\t|");
+		System.out.println("|\t\t\t	Borelasgamuwa	\t\t\t|");
+		System.out.println("+=======================================================================+");
+		System.out.println("\n");
+	}
+}
